273d3f47881d98287b06105cb0895394cdf83fec
William Griebeler <williamgriebeler10@gmail.com>
2024-04-09 19:30:36 -0300

    commit

---
+++ src/Exercicio.java
@@ -0,0 +1,14 @@
+public class Exercicio {
+    
+    public static void main(String[] args) throws Exception {
+        
+        int idade = 25;
+        System.out.printf("eu tenho %d anos de idade", idade);
+
+
+        
+
+
+    }
+
+}
